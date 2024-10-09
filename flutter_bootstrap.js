6f4ac794629257e4d7e51ee761ab8f6f4c2c137c
publishedproject <contentclaim101@gmail.com>
2024-10-09 16:40:41 +0530

    Added Category Section

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "188324991"
+    serviceWorkerVersion: "930974494"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,14 +26,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "373234d664ffe656d0fb4039a86ad513
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "c6699d46b605a35dd2aab73567f8400d",
+"flutter_bootstrap.js": "acca0428d8f7547f8f9e382be667d205",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "dbfd23a47528ce029a98b60bb6bc73a8",
 "/": "dbfd23a47528ce029a98b60bb6bc73a8",
-"main.dart.js": "8aca5373a0fc57b3069ee509018d187b",
+"main.dart.js": "0a4bf243e6bc54d7582d464db497c817",
 "manifest.json": "343f7176c2555e1464338eb44303ffe6",
 "version.json": "4c05dddf87e1c3f5f4dd20b92f4f2ed4"};
 // The application shell files that are downloaded before a service worker can
